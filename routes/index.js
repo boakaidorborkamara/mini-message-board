f0117f82131e611d4f6ad7df48e6e2f2d00bfe6b
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2022-02-23 17:31:44 +0000

    added and styled the home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+// data object 
+let messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: "Message Board", messages: messages});
+  // res.send(messages);
 });
 
 module.exports = router;
